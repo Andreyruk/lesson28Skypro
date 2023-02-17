8655c24c04b21861cfa9abbea7d5faddd98863b1
MakTub <7andreyru@mail.ru>
2023-02-17 18:07:05 +0500

    ДЗ к уроку 28

---
+++ src/main/java/pro/sky/lesson28skypro/Lesson28SkyproApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.lesson28skypro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lesson28SkyproApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lesson28SkyproApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/lesson28skypro/Lesson28SkyproApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.lesson28skypro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lesson28SkyproApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
